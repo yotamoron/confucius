0fcdbee3bfe12492e1f3ef89d26d9cd9dc79a29a
Roy Bass <rbass@rbass-pc.(none)>
2013-09-02 13:37:14 +0300

    One file for handling fields

--- confucius/static/js/fields.js
+++ confucius/static/js/fields.js
@@ -1,6 +1,26 @@
+var model = {}
 
+function saveModelValue(id, key, value) {
+    if (model[id] === undefined) {
+        model[id] = {};
+    }
+    model[id][key] = value; 
+}
 
+function loadModelValue(id, key) {
+    if (model[id] === undefined) {
+        return "";
+    }
+    return model[id][key];
+}
 
+function loadModel(id) {
+    return model[id];
+}
+
+function saveModel(id, fields) {
+    model[id] = fields;
+}
 
 
 function getSelectService(name) {
